8a0b0c5460310b5a6c2de11dcd468dd552785dce
MonomI <monomax_xxi@mail.ru>
2013-03-12 21:23:00 +0400

    second commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,9 @@ class SessionsController < ApplicationController
   def new
     srand
     session[:state] ||= Digest::MD5.hexdigest(rand.to_s)
-    
+	    
     @vk_url = VkontakteApi.authorization_url(scope: [:friends, :groups, :offline, :notify], state: session[:state])
+	
   end
   
   def callback
